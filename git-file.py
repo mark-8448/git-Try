30bc31f55a6f0c03def6bb45f5c4be10d61d87a0
mark-8448 <markgladkov1@gmail.com>
2020-04-24 22:50:10 +0300

    v.1

--- git-file.py
+++ git-file.py
@@ -1,5 +1,5 @@
 import decimal
 with decimal.localcontext() as ctx:
-    ctx.prec = 2  # знаки после запятой
+    ctx.prec = 3  # знаки после запятой
     print(decimal.Decimal('1.00') / decimal.Decimal('3.00'))
-# 0.33
+# 0.333
